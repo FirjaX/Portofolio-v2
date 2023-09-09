744f1ec47ecaf8cad46287c72e1db7d32557cd16
FirjaX <107788805+FirjaX@users.noreply.github.com>
2023-09-09 22:21:19 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
           <span class="self-center text-lg text-[#ffdb70] font-semibold whitespace-nowrap fadein-bot hover:text-amber-100">Ares();</span>
         </button>
         <div class="flex md:order-2 fadein-bot">
-          <a href="https://instagram.com/genzai66"><img class="w-9 rounded-full" src="https://telegra.ph/file/60b56d609ed2222a508b8.jpg" alt="instagram"></a>
+          <a href="https://wa.me/6281802626072"><img class="w-9 rounded-full" src="https://telegra.ph/file/5a1051bfda66c361a3796.jpg" alt="whatsapp"></a>
         </div>
         <div class="hidden md:flex justify-between items-center w-full md:w-auto md:order-1" id="mobile-menu-3">
           <ul class="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
